6aed1ab233e6091ee141d2a3cd6bec3e44a9e964
peiq <88368889+nessqing@users.noreply.github.com>
2022-09-17 16:25:35 +0800

    Initial Commit

---
+++ C_project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_project
+//
+//  Created by 黃沛晴 on 2022/9/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
